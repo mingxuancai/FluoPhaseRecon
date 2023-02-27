a9eb4a5c52901b252320a78c0d13f3e07dda46c1
Mingxuan Cai <mingxuan_cai@berkeley.edu>
2023-02-27 09:57:36 -0800

    gpu success recon

--- opticaltomography/.ipynb_checkpoints/loss-checkpoint.py
+++ opticaltomography/.ipynb_checkpoints/loss-checkpoint.py
@@ -8,6 +8,7 @@ def mse_loss(predict, obj, device='cpu'):
     
     y, x = obj.size()
     mse = torch.pow(predict-obj, 2).sum()/(y*x)
+    # print(mse.device)
     return mse
 
 
@@ -20,11 +21,12 @@ def sparsity_loss(obj, weight, device='cpu'):
 
 def total_variation_loss(obj, weight, device='cpu'):
     y, x, z = obj.size()
-    tv_h = torch.pow(obj[1,:,:] - obj[-1,:,:], 2).sum()
-    tv_w = torch.pow(obj[:,1,:] - obj[:,-1,:], 2).sum()
-    tv_z = torch.pow(obj[:,:,1] - obj[:,:,-1], 2).sum()
+    tv_h = torch.abs(obj[1,:,:] - obj[-1,:,:]).sum()
+    tv_w = torch.abs(obj[:,1,:] - obj[:,-1,:]).sum()
+    tv_z = torch.abs(obj[:,:,1] - obj[:,:,-1]).sum()
     
     loss = weight*(tv_h+tv_w+tv_z)/(y*x*z)
+    loss = loss.to(device)
     
     return loss
 
